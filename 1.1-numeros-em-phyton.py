8e170086639018bb4b07977af6896542fad83adc
Vinícius Leonardo Lima <vinicius.leonardo.lima2009@gmail.com>
2026-05-06 11:25:28 -0300

    meu primeiro programa phyton

---
+++ 1.1-numeros-em-phyton.py
@@ -0,0 +1,33 @@
+# AULA COMPLETA: NUMEROS EM PHYTON
+
+"""
+  Vamos aprender:
+1 - Tipos Númericos
+2 - Conversões de Tipos
+3 - Hierarquia Númerica
+4 - Operações Matemáticas
+5 - Coerção de Tipos
+6 - Verificação de Tipos
+7 - Entrada de Dados
+"""
+#=============================
+## PASSO 01 - TIPOS NÚMERICOS
+#=============================
+#INT  -> NÚMEROS INTEIROS
+# float  -> NÚMEROS COM CASAS DECIMAIS
+# complex -> NÚMEROS compexos (usado em matemática /engenharia)
+
+print ("===== TIPOS NÚMERICOS =====")
+
+# EXEMPLO 01 - NÚMERO INTEIRO
+#criamos uma váriavel chamada de número_inteiro
+numero_inteiro = 10
+
+#Mostramos o valor
+print ("Valor:", numero_inteiro)
+
+#Type () mostra qual é o tipo da variavél
+print ("Tipo:", type(numero_inteiro))
+
+print ("------------------------------")
+
